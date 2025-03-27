eb22357ca6c5746618cc90357d0554a5b5ce132b
Priyanshup9788 <167433113+Priyanshup9788@users.noreply.github.com>
2025-03-27 05:21:28 +0000

    search updated

--- routing/src/Components/View.jsx
+++ routing/src/Components/View.jsx
@@ -11,48 +11,68 @@ const View = () => {
 
   const [filter, setFilter] = useState("");
 
-  const [perPage,setPerPage]=useState(3);
-  const [currentPage,setCurrentPage]=useState(1);
-  const [totalpages,setTotalPages]=useState(0);
+  const [perPage, setPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalpages, setTotalPages] = useState(0);
 
-  const [virtualEmp,setVirtualEmp]=useState([]);
+  const [virtualEmp, setVirtualEmp] = useState([]);
+
+  useEffect(() => {
+    let lastIndex = perPage * currentPage;
+    let firstIndex = lastIndex - perPage;
+
+    let newEmp = [...allEmp];
+    let pages = Math.ceil(newEmp.length / perPage);
+    setTotalPages(pages);
+    newEmp = newEmp.slice(firstIndex, lastIndex);
+    setVirtualEmp(newEmp);
+  }, [currentPage, allEmp])
+
 
   const onDelete = (e, id) => {
     e.preventDefault();
-    let oldEmp=JSON.parse(localStorage.getItem("emp"));
-    oldEmp=oldEmp.filter((emp)=>emp.id!= id);
-    
+    let oldEmp = JSON.parse(localStorage.getItem("emp"));
+    oldEmp = oldEmp.filter((emp) => emp.id != id);
+
     setAllEmp(oldEmp);
-    localStorage.setItem("emp",JSON.stringify(oldEmp))
+    localStorage.setItem("emp", JSON.stringify(oldEmp))
   }
 
-  const onSortChange=(e)=>{
-    e.preventDefault();
-    
-    let newEmp = [...allEmp];
-    if(e.target.value=="asc")
-    {
-      newEmp.sort((a,b)=>(a.name.localeCompare(b.name)));
+  const onFilterChange = (e) => {
+    let newdata = allEmp.filter((emp) => {
+      if (e.target.value != "") {
+        if (emp.name.toLowerCase().includes(e.target.value.toLowerCase())) {
+          return emp;
+        }
+      }
+    });
+    if (e.target.value == "") {
+      setAllEmp(() => {
+        let epmloyes = localStorage.getItem("emp");
+
+        return epmloyes ? JSON.parse(epmloyes) : [];
+      });
+      return;
     }
-    else if(e.target.value=="desc"){
-      newEmp.sort((a,b)=>(b.name.localeCompare(a.name)));
+    setAllEmp(newdata);
+  }
+
+  const onSortChange = (e) => {
+    e.preventDefault();
+
+    let newEmp = [...allEmp];
+    if (e.target.value == "asc") {
+      newEmp.sort((a, b) => (a.name.localeCompare(b.name)));
+    }
+    else if (e.target.value == "desc") {
+      newEmp.sort((a, b) => (b.name.localeCompare(a.name)));
     }
     setAllEmp(newEmp);
   }
 
-  useEffect(()=>{
-    let lastIndex = perPage * currentPage;
-    let firstIndex = lastIndex-perPage;
-
-    let newEmp = [...allEmp];
-    let pages = Math.ceil(newEmp.length/perPage);
-    setTotalPages(pages);
-    newEmp=newEmp.slice(firstIndex,lastIndex);
-    setVirtualEmp(newEmp);
-  },[currentPage,allEmp])
 
 
- 
+
 
   return (
     <div className="container">
@@ -60,8 +80,8 @@ const View = () => {
       <table>
         <thead>
           <tr>
-            <td > <input type="text" onChange={(e) => (setFilter(e.target.value))} /></td>
-            <td colSpan={6}><select name="sorting" onChange={(e)=>{onSortChange(e)}}>
+            <td > <input type="text" onChange={(e) => (onFilterChange(e))} /></td>
+            <td colSpan={6}><select name="sorting" onChange={(e) => { onSortChange(e) }}>
               <option value="">--select sort --</option>
               <option value="asc">Acending</option>
               <option value="desc">Decending</option>
@@ -79,16 +99,7 @@ const View = () => {
         </thead>
 
         <tbody>
-          {virtualEmp.filter((emp) => {
-            if (filter != "") {
-              if (emp.name.includes(filter)) {
-                return emp;
-              }
-            }
-            else {
-              return emp;
-            }
-          }).map((emp, i) => (
+          {virtualEmp.map((emp, i) => (
             <tr key={i}>
               <td id="displayName">{emp.name}</td>
               <td id="displayEmail">{emp.email}</td>
@@ -103,14 +114,18 @@ const View = () => {
         </tbody>
       </table>
       <div class="pagination">
-            <button id="prevPage" onClick={()=>{if(currentPage!=1){
-              setCurrentPage(currentPage-1);
-            }}}>Previous</button>
-            <span id="pageNumber">Page {currentPage}</span>
-            <button id="nextPage"  onClick={()=>{if(currentPage!=totalpages){
-              setCurrentPage(currentPage+1);
-            }}}>Next</button>
-        </div>
+        <button id="prevPage" onClick={() => {
+          if (currentPage != 1) {
+            setCurrentPage(currentPage - 1);
+          }
+        }}>Previous</button>
+        <span id="pageNumber">Page {currentPage}</span>
+        <button id="nextPage" onClick={() => {
+          if (currentPage != totalpages) {
+            setCurrentPage(currentPage + 1);
+          }
+        }}>Next</button>
+      </div>
     </div>
   )
 }
